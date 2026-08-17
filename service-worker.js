6b50e1057b0ce8ffc7790549f1e99ae4510f6822
raycbn <74503112+raycbn@users.noreply.github.com>
2026-08-17 09:02:09 +0200

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "instagram-unfollowers-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache =>
+                cache.addAll(FILES)
+            )
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(cached => {
+
+                return cached ||
+                    fetch(event.request);
+
+            })
+
+    );
+
+});
